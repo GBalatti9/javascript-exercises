8c0de06f9bb31ea371decbe03a3c5a46729291b9
GBalatti9 <gas.balatti@gmail.com>
2023-07-31 20:25:54 -0300

    Excercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+    console.log(num);
+    let newArray = [];
 
+    arr.forEach(number => {
+        if(!num.includes(number)){
+            newArray.push(number)
+        }
+    })
+
+    return newArray
 };
+
+// console.log(removeFromArray([1,2,3], 2, 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;

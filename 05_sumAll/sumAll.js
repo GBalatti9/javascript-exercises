b18b0c6ca52fbe4cd7dd9cac09e539eabeef0678
GBalatti9 <gas.balatti@gmail.com>
2023-07-31 20:39:03 -0300

    Excercise five

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number"){ return "ERROR" }
+    let smallNumber = Math.min(a, b);
+    let bigNumber = Math.max(a, b);
+    let acumulate = 0;
+    for (let i = smallNumber; i <= bigNumber; i++) {
+        acumulate += i
+    }
+    return acumulate
 };
 
 // Do not edit below this line
